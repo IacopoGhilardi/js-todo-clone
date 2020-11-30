85879f82a0617926cee0071f866ebb609726c1b4
Iacopo <iacopo.ghila@gmail.com>
2020-11-30 16:18:20 +0100

    Strutturata

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 $(document).ready(function(){
 
+
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+
+    //al click sul pulsante add aggiungo una nuova task in pagina
+    $('#add').click(function(){
+
+        console.log('ciao');
+        var userInput = $('.add-task input').val();
+
+        var context = {
+            "content": userInput
+        }
+
+        var html = template(context);
+        $('.to-do').append(html);
+    });
+
     
+    //al click sull'icon X rimuovo la task in pagina
+    $(document).on('click', '.fa-times-circle', function(){
+        $(this).parent().remove();
+    });
+
 });
